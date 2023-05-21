1535a53d8b94471ae2fffb95d84a90e069a38c0e
Noé Vila <noevilamu@gmail.com>
2023-05-21 18:26:58 +0200

    Refactored and observableobject learned

---
+++ WatchTFM Watch App/HealthKitManager.swift
@@ -0,0 +1,68 @@
+//
+//  File.swift
+//  WatchTFM Watch App
+//
+//  Created by Noe Vila Muñoz on 21/5/23.
+//
+
+import Foundation
+import HealthKit
+import SwiftUI
+
+class HealthKitManager: ObservableObject {
+    private let healthStore = HKHealthStore()
+    let heartRateQuantity = HKUnit(from: "count/min")
+    @Published var currentValue = 0
+    
+    func start() {
+        autorizeHealthKit()
+        startHeartRateQuery(quantityTypeIdentifier: .heartRate)
+    }
+    
+    func autorizeHealthKit() {
+        let healthKitTypes: Set = [
+            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
+        ]
+        
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { _, _ in }
+    }
+    
+    private func startHeartRateQuery(quantityTypeIdentifier: HKQuantityTypeIdentifier) {
+        
+        // 1
+        let devicePredicate = HKQuery.predicateForObjects(from: [HKDevice.local()])
+        // 2
+        let updateHandler: (HKAnchoredObjectQuery, [HKSample]?, [HKDeletedObject]?, HKQueryAnchor?, Error?) -> Void = {
+            query, samples, deletedObjects, queryAnchor, error in
+            
+            // 3
+        guard let samples = samples as? [HKQuantitySample] else {
+            return
+        }
+            
+        self.process(samples, type: quantityTypeIdentifier)
+
+        }
+        
+        // 4
+        let query = HKAnchoredObjectQuery(type: HKObjectType.quantityType(forIdentifier: quantityTypeIdentifier)!, predicate: devicePredicate, anchor: nil, limit: HKObjectQueryNoLimit, resultsHandler: updateHandler)
+        
+        query.updateHandler = updateHandler
+        
+        // 5
+        
+        healthStore.execute(query)
+    }
+    
+    private func process(_ samples: [HKQuantitySample], type: HKQuantityTypeIdentifier) {
+        var lastHeartRate = 0.0
+        
+        for sample in samples {
+            if type == .heartRate {
+                lastHeartRate = sample.quantity.doubleValue(for: heartRateQuantity)
+            }
+            self.currentValue = Int(lastHeartRate)
+        }
+        
+    }
+}
